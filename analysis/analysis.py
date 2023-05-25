31a28f2a2db3ce351a250bdd8a8f7a2c544420d5
mshabanov27 <90135657+mshabanov27@users.noreply.github.com>
2023-05-26 00:22:44 +0400

    Analytics pie chart (#2)
    * Pie chart modified
    
    * New functions added, plot became smarter

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -13,16 +13,22 @@ def normalize_history(json_data):
     return df
 
 
-def visualize_top_artists(json_data):
-    streaming_history = normalize_history(json_data)
-    artists_count = streaming_history.groupby(['Artist']).agg(Count=('Artist', 'count')).reset_index()
-    artists_count.rename(columns={'Count': 'Tracks listened'}, inplace=True)
+def make_others_section(artists_count):
+    last_rows = artists_count.head(12).tail(3)
+    column_sum = last_rows['Tracks listened'].sum()
+    tracks_sum = artists_count['Tracks listened'].head(12).sum()
 
-    color_continuous_scale = ['#2F0F43', '#522278', '#613D9C']
-    fig = px.pie(artists_count, values='Tracks listened', names='Artist',
-                 color_discrete_sequence=px.colors.diverging.PRGn, hole=0.65)
-    fig.update_traces(textfont=dict(size=25))
+    if column_sum / tracks_sum <= 0.12:
+        artists_count = artists_count.head(12)
+        artists_count.drop(artists_count.tail(3).index, inplace=True)
+        new_row = pd.DataFrame({'Artist': 'Others', 'Tracks listened': column_sum}, index=[len(artists_count)])
+        artists_count = pd.concat([artists_count, new_row])
+        return artists_count.sort_values(by=['Tracks listened'], ascending=False)
+    else:
+        return artists_count.head(10)
 
+
+def draw_circles():
     big_circle = dict(
         type='circle',
         xref='paper', yref='paper',
@@ -37,14 +43,32 @@ def visualize_top_artists(json_data):
         line_color='grey', line_width=5
     )
 
+    return big_circle, small_circle
+
+
+def visualize_top_artists(json_data):
+    streaming_history = normalize_history(json_data)
+    artists_count = streaming_history.groupby(['Artist']).agg(Count=('Artist', 'count')).reset_index()
+    artists_count.rename(columns={'Count': 'Tracks listened'}, inplace=True)
+
+    artists_count = make_others_section(artists_count)
+
+    color_continuous_scale = ['#2f0f43', '#39194f', '#43235c', '#4d2d68', '#573776',
+                              '#614283', '#6c4c91', '#76579e', '#8063ad', '#8b6ebb']
+
+    fig = px.pie(artists_count, values='Tracks listened', names='Artist',
+                 color_discrete_sequence=color_continuous_scale, hole=0.65)
+    fig.update_traces(textfont=dict(size=25), hovertemplate=' <br>   %{label}   <br> ')
+
+    big_circle, small_circle = draw_circles()
+
     fig.update_layout(
         {
             'plot_bgcolor': 'rgba(0,0,0,0)',
             'paper_bgcolor': 'rgba(0,0,0,0)'
         },
         shapes=[big_circle, small_circle], showlegend=False,
-        hoverlabel=dict(bgcolor="black", font_size=16, font_family="Helvetica", align='auto')
+        hoverlabel=dict(bgcolor="black", font_size=20, font_family="Helvetica")
     )
 
     fig.show()
-    plotly.offline.plot(fig, filename='profile.html', config={'displayModeBar': False})

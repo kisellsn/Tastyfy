baf55466b303511f72120bb4e1bb96d5ec59e11a
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-05-28 12:42:58 +0200

    Spotify

--- frontend/src/util/functions.js
+++ frontend/src/util/functions.js
@@ -1,31 +1,19 @@
 import axios from 'axios';
 
-export function removeHashParams(value) {
-    if (typeof value !== 'string') return '';
-    const split = value.split('#');
-    return split[0];
-  }
-const config = { 
-    headers: {  
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  }
-export const registerSpotify = async (code) => {
+export const registerSpotify = async () => {
     try {
-        const response = await axios.post('/api/user', { code: removeHashParams(code) }, config);
+        const response = await axios.get('/api/user');
         return response.data;
     } catch (error) {
-        console.error(error);
-        throw error;
+        console.error('Error',error);
     }
     };
-
+    
 export const getToken = async () => {
     try {
         const response = await axios.get('/token');
         return response.data;
     } catch (error) {
-        console.error('404', error);
+        console.error('Error', error);
     }
     };

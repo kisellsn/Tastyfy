442e2b482b04491a468298eb9ebb125b058fa396
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-05-29 16:21:33 +0200

    top and rec

--- frontend/src/components/PlotComponent/PlotComponent.jsx
+++ frontend/src/components/PlotComponent/PlotComponent.jsx
@@ -1,25 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import Plot from 'react-plotly.js';
+// import Plot from 'react-plotly.js';
 import { sircleDiagram } from 'src/util/functions';
 
 function PlotComponent() {
   const [plotData, setPlotData] = useState(null);
 
   useEffect(() => {
-    setPlotData(sircleDiagram())
+    async function fetchData() {
+      try {
+        const data = await sircleDiagram();
+        setPlotData(data);
+      } catch (error) {
+        console.error('Error fetching plot data:', error);
+      }
+    }
+
+    fetchData();
   }, []);
 
-
   return (
-    <div style={{width: '100%', position:'relative'}}>
+    <div style={{ width: '100%', position: 'relative' }}>
       <h2>Plot</h2>
       {plotData ? (
-        <div>
-            <Plot
-                data={plotData.data} // Assuming plotData has a 'data' property with plot data
-                layout={plotData.layout} // Assuming plotData has a 'layout' property with layout options
-                style={{width: '100%', position: 'relative'}}
-            />
+        <div
+          style={{ width: '200px', height: '200px', position: 'absolute' }}
+          dangerouslySetInnerHTML={{ __html: plotData }}
+        >
+          {/* <Plot
+            data={plotData.data} // Assuming plotData has a 'data' property with plot data
+            style={{ width: '100%', position: 'relative' }}
+          /> */}
         </div>
       ) : (
         <p>Loading plot data...</p>

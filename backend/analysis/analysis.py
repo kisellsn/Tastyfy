e9c06be2aed4b83864c0c61eba990d7bcb5ba693
mshabanov27 <90135657+mshabanov27@users.noreply.github.com>
2023-05-31 15:01:42 +0400

    1 to 0

--- backend/analysis/analysis.py
+++ backend/analysis/analysis.py
@@ -55,7 +55,7 @@ def visualize_genres_barchart(genres_complex_list):
 
 def generate_genres_text(genres_complex_list):
     genres = convert_genres(genres_complex_list)
-    best_genre = genres.loc[1, :].values.flatten().tolist()
+    best_genre = genres.loc[0, :].values.flatten().tolist()
     return f'You really love listening to {best_genre[0]}! You`ve listened to it {best_genre[1]} times lately'
 
 

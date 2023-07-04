91391c6fbc4e0cc4b99c97fe7589b07278faea64
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-07-04 22:06:00 +0200

    about,privacy responsive

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -15,81 +15,85 @@ function About(props) {
   };
   return (
     <div id='mainA' className={props.className}>
-      <div id='SpotifyLogoS'></div>
-      <div id='leftRectangle'>
-        <div id='tastyfy'>{`Tastyfy`}</div>
-        <div id='backRectangle' onClick={handleGoBack}>
-            <div id='q1'>
-                <img id='vector1' src={Vector1Image} loading="lazy" alt={'Vector'} />
+      <div className='containerA'>
+        <div id='leftRectangleA'>
+          <div id='tastyfyA'>{`Tastyfy`}</div>
+          <div id='backRectangleA' onClick={handleGoBack}>
+              <img id='vector1A' src={Vector1Image} loading="lazy" alt={'Vector'} />
+              <div id='backA'>Back</div>
+          </div>
+        </div>
+        <div className='about-body'>
+          <div className='team'>
+            <div className='team-card'>
+              <img 
+                src={PumaImage}
+                loading="lazy"
+                alt={'Sonya Kondratska'}
+              />
+              <div className='team-card-info'>
+                <h3>Sonya Kondratska</h3>
+                <div className='team-card-rectangle'></div>
+                <h3>Back-end Developer</h3>
+              </div>
             </div>
-            <div id='back'>Back</div>
+            <div className='team-card'>
+              <img 
+                src={SnakeImage} 
+                loading="lazy" 
+                alt={'Andrii Mieshkov'}
+              />
+              <div className='team-card-info'>
+                <h3>Andrii Mieshkov</h3>
+                <div className='team-card-rectangle'></div>
+                <h3>Front-end Developer</h3>
+              </div>
+            </div>
+            <div className='team-card'>
+              <img 
+                src={SpiderImage} 
+                loading="lazy" 
+                alt={'Tykhonenko Mykyta'}   
+              />
+              <div className='team-card-info'>
+                <h3>Tykhonenko Mykyta</h3>
+                <div className='team-card-rectangle'></div>
+                <h3>UI/UX Designer</h3>
+              </div>
+            </div>
+            <div className='team-card'>
+              <img 
+                src={PenguinImage} 
+                loading="lazy" 
+                alt={'Shabanov Metin'}
+              />
+              <div className='team-card-info'>
+                <h3>Shabanov Metin</h3>
+                <div className='team-card-rectangle'></div>
+                <h3>Data Analyst</h3>
+              </div>
+            </div>
+          </div>
+          <div id='descriptionA'>
+            <h2>About</h2>
+            <div id='textAbout'>
+              <p>Welcome to Tastyfy!</p>
+              <p>Tastyfy is a revolutionary music preference analyzer designed to help you delve deeper into your musical tastes and uncover new music that resonates with your personal preferences. With our innovative platform, we bring together a team of talented individuals who are passionate about music and technology to enhance your music discovery journey.</p>
+              <h3>Meet Our Team</h3>
+              <h4>Sonya Kondratska - Back-end Developer</h4>
+              <p>Sonya is the mastermind behind the technical infrastructure that powers Tastyfy. With her expertise in back-end development, she ensures that our platform operates smoothly, securely, and efficiently. She is committed to delivering a seamless user experience, enabling you to explore your music preferences effortlessly.</p>
+              <h4>Andrii Mieshkov - Front-end Developer</h4>
+              <p>Andrii is responsible for the visual and interactive aspects of Tastyfy. Using his skills in front-end development, he creates an intuitive and engaging user interface that makes navigating through the platform a delightful experience. Andrii works tirelessly to bring your music preferences to life and ensure that you have a seamless interaction with Tastyfy.</p>
+              <h4>Tykhonenko Mykyta - UI/UX Designer</h4>
+              <p>Tykhonenko's expertise lies in crafting captivating user experiences. As the UI/UX designer for Tastyfy, he combines aesthetics with functionality, designing a visually appealing interface that complements the overall user journey. Tykhonenko's goal is to make your time on Tastyfy enjoyable, visually stimulating, and easy to navigate.</p>
+              <h4>Shabanov Metin - Data Analyst</h4>
+              <p>Metin is the brains behind the data analysis at Tastyfy. With his analytical skills and music expertise, he dives into the vast world of music data to uncover hidden connections and patterns. By analyzing your music preferences, Metin continuously enhances the algorithms that power Tastyfy, ensuring that the music recommendations you receive are tailored specifically to your unique taste.</p>
+              <p>At Tastyfy, we believe that music has the power to enrich our lives and connect us on a deeper level. Our mission is to empower you to explore your music preferences, expand your musical horizons, and make discovering new music an exciting and personalized journey.</p>
+              <p>Whether you're a seasoned music aficionado or just beginning to explore the vast world of music, Tastyfy is here to accompany you on your musical adventure. So, dive in, uncover your musical preferences, and let Tastyfy be your guide to a world of endless musical possibilities.</p>
+            </div>
+          </div>
         </div>
       </div>
-        <img id='puma'
-            src={PumaImage}
-            loading="lazy"
-            alt={'Sonya Kondratska'}/>
-        <img id='snake'
-            src={SnakeImage} 
-            loading="lazy" 
-            alt={'Andrii Mieshkov'} />
-        <img id='spider'
-            src={SpiderImage} 
-            loading="lazy" 
-            alt={'Tykhonenko Mykyta'} />
-        <img id='penguin'
-            src={PenguinImage} 
-            loading="lazy" 
-            alt={'Shabanov Metin'} />
-
-
-        <div id='sonyaKondratskaBackE'>
-            <span id='sonyaKondratskaBackESpan1'>{`Sonya Kondratska
-
-`}</span>
-            <span id='sonyaKondratskaBackESpan2'>{`Back-end Developer`}</span>
-            <div id='rectangle60'></div>
-        </div>
-        <div id='andriiMieshkovFrontE'>
-            <span id='andriiMieshkovFrontESpan1'>{`Andrii Mieshkov
-
-`}</span>
-            <span id='andriiMieshkovFrontESpan2'>{`Front-end Developer`}</span>
-            <div id='rectangle61'></div>
-        </div>
-        <div id='tykhonenkoMykytaUiUx'>
-            <span id='tykhonenkoMykytaUiUxSpan1'>{`Tykhonenko Mykyta
-
-`}</span>
-            <span id='tykhonenkoMykytaUiUxSpan2'>{`UI/UX Designer`}</span>
-            <div id='rectangle62'></div>
-        </div>
-        <div id='shabanovMetinDataAna'>
-            <span id='shabanovMetinDataAnaSpan1'>{`Shabanov Metin
-
-`}</span>
-            <span id='shabanovMetinDataAnaSpan2'>{`Data Analyst`}</span>
-            <div id='rectangle63'></div>
-        </div>
-      <div id='description'>
-        <div id='about'>{`About`}</div>
-        <div id='textAbout'>
-          <p>Welcome to Tastyfy!</p>
-          <p>Tastyfy is a revolutionary music preference analyzer designed to help you delve deeper into your musical tastes and uncover new music that resonates with your personal preferences. With our innovative platform, we bring together a team of talented individuals who are passionate about music and technology to enhance your music discovery journey.</p>
-          <h3>Meet Our Team</h3>
-          <h4>Sonya Kondratska - Back-end Developer</h4>
-          <p>Sonya is the mastermind behind the technical infrastructure that powers Tastyfy. With her expertise in back-end development, she ensures that our platform operates smoothly, securely, and efficiently. She is committed to delivering a seamless user experience, enabling you to explore your music preferences effortlessly.</p>
-          <h4>Andrii Mieshkov - Front-end Developer</h4>
-          <p>Andrii is responsible for the visual and interactive aspects of Tastyfy. Using his skills in front-end development, he creates an intuitive and engaging user interface that makes navigating through the platform a delightful experience. Andrii works tirelessly to bring your music preferences to life and ensure that you have a seamless interaction with Tastyfy.</p>
-          <h4>Tykhonenko Mykyta - UI/UX Designer</h4>
-          <p>Tykhonenko's expertise lies in crafting captivating user experiences. As the UI/UX designer for Tastyfy, he combines aesthetics with functionality, designing a visually appealing interface that complements the overall user journey. Tykhonenko's goal is to make your time on Tastyfy enjoyable, visually stimulating, and easy to navigate.</p>
-          <h4>Shabanov Metin - Data Analyst</h4>
-          <p>Metin is the brains behind the data analysis at Tastyfy. With his analytical skills and music expertise, he dives into the vast world of music data to uncover hidden connections and patterns. By analyzing your music preferences, Metin continuously enhances the algorithms that power Tastyfy, ensuring that the music recommendations you receive are tailored specifically to your unique taste.</p>
-          <p>At Tastyfy, we believe that music has the power to enrich our lives and connect us on a deeper level. Our mission is to empower you to explore your music preferences, expand your musical horizons, and make discovering new music an exciting and personalized journey.</p>
-          <p>Whether you're a seasoned music aficionado or just beginning to explore the vast world of music, Tastyfy is here to accompany you on your musical adventure. So, dive in, uncover your musical preferences, and let Tastyfy be your guide to a world of endless musical possibilities.</p>
-        </div>
-      </div>
-
     </div>
   );
 }
--- frontend/src/components/Privacy/Privacy.jsx
+++ frontend/src/components/Privacy/Privacy.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Vector1Image from 'src/assets/images/about_Vector_1.png';
-import './styles.scss'
 import { useNavigate } from 'react-router-dom';
 
 function Privacy(props) {
@@ -10,63 +9,62 @@ function Privacy(props) {
     navigate('/menu');
   };
   return (
-    <div id='mainP' className={props.className}>
-      <div id='SpotifyLogoS'></div>
-      <div id='leftRectangle'>
-        <div id='tastyfy'>{`Tastyfy`}</div>
-        <div id='backRectangle' onClick={handleGoBack}>
-            <div id='q1'>
-                <img id='vector1' src={Vector1Image} loading="lazy" alt={'Vector'} />
+    <div id='mainA' className={props.className}>
+      <div className='containerA'>
+        <div id='leftRectangleA'>
+          <div id='tastyfyA'>{`Tastyfy`}</div>
+          <div id='backRectangleA' onClick={handleGoBack}>
+            <img id='vector1A' src={Vector1Image} loading="lazy" alt={'Vector'} />
+            <div id='backA'>Back</div>
+          </div>
+        </div>
+        <div className='about-body'>
+          <div id='descriptionA'>
+            <h2>Privacy</h2>
+            <div id='textAbout'>
+              Effective Date: 31.05.2023
+              <br/><br/>
+              This Privacy Policy describes how Tastyfy ("we," "us," or "our") collects, uses, and shares information when you use our music tastes analyzer application ("App"). By using the App, you consent to the practices described in this Privacy Policy.
+              <br/><br/>
+              <strong>Information We Collect</strong>
+              <br/><br/>
+              Personal Information: We do not collect any personally identifiable information (such as your name, email address, or contact information) through the App.
+              <br/><br/>
+              Usage Information: We may collect certain non-personal information automatically when you use the App. This may include your device's unique identifier, IP address, operating system information, and usage statistics.
+              <br/><br/>
+              Analyzed Data: The App analyzes your music tastes based on the data you provide, such as your music streaming history or preferences. This data is processed locally on your device and is not transmitted to our servers.
+              <br/><br/>
+              <strong>Use of Information</strong>
+              <br/><br/>
+              Personal Information: Since we do not collect personal information, we do not use it for any purpose.
+              <br/><br/>
+              Usage Information: We may use usage information to improve the functionality and performance of the App, troubleshoot issues, and analyze trends.
+              <br/><br/>
+              Analyzed Data: The analyzed data is used solely to provide you with personalized music recommendations and insights within the App. It is not shared with any third parties.
+              <br/><br/>
+              <strong>Data Security</strong>
+              <br/><br/>
+              We take reasonable measures to protect the security of your information. However, please be aware that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+              <br/><br/>
+              <strong>Third-Party Services</strong>
+              <br/><br/>
+              The App may include links to third-party websites or services that are not operated by us. These third-party services have their own privacy policies, and we have no control over and assume no responsibility for their content, privacy policies, or practices.
+              <br/><br/>
+              <strong>Children's Privacy</strong>
+              <br/><br/>
+              The App is not intended for children under the age of 13. We do not knowingly collect personal information from children under 13. If you are a parent or guardian and believe that your child has provided personal information, please contact us, and we will promptly delete such information from our records.
+              <br/><br/>
+              <strong>Changes to This Privacy Policy</strong>
+              <br/><br/>
+              We reserve the right to update or modify this Privacy Policy at any time. We will notify you of any changes by posting the updated Privacy Policy within the App. Your continued use of the App after any modifications indicate your acceptance of the updated Privacy Policy.
+              <br/><br/>
+              <strong>Contact Us</strong>
+              <br/><br/>
+              If you have any questions, concerns, or suggestions regarding this Privacy Policy, please contact us at tastyfy.tech@gmail.com.
             </div>
-            <div id='back'>Back</div>
+          </div>
         </div>
       </div>
-      <div id='descriptionP'>
-        <div id='privacy'>{`Privacy
-        `}</div>
-        <div id='textPrivacy'>
-          Effective Date: 31.05.2023
-          <br/><br/>
-          This Privacy Policy describes how Tastyfy ("we," "us," or "our") collects, uses, and shares information when you use our music tastes analyzer application ("App"). By using the App, you consent to the practices described in this Privacy Policy.
-          <br/><br/>
-          <strong>Information We Collect</strong>
-          <br/><br/>
-          Personal Information: We do not collect any personally identifiable information (such as your name, email address, or contact information) through the App.
-          <br/><br/>
-          Usage Information: We may collect certain non-personal information automatically when you use the App. This may include your device's unique identifier, IP address, operating system information, and usage statistics.
-          <br/><br/>
-          Analyzed Data: The App analyzes your music tastes based on the data you provide, such as your music streaming history or preferences. This data is processed locally on your device and is not transmitted to our servers.
-          <br/><br/>
-          <strong>Use of Information</strong>
-          <br/><br/>
-          Personal Information: Since we do not collect personal information, we do not use it for any purpose.
-          <br/><br/>
-          Usage Information: We may use usage information to improve the functionality and performance of the App, troubleshoot issues, and analyze trends.
-          <br/><br/>
-          Analyzed Data: The analyzed data is used solely to provide you with personalized music recommendations and insights within the App. It is not shared with any third parties.
-          <br/><br/>
-          <strong>Data Security</strong>
-          <br/><br/>
-          We take reasonable measures to protect the security of your information. However, please be aware that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
-          <br/><br/>
-          <strong>Third-Party Services</strong>
-          <br/><br/>
-          The App may include links to third-party websites or services that are not operated by us. These third-party services have their own privacy policies, and we have no control over and assume no responsibility for their content, privacy policies, or practices.
-          <br/><br/>
-          <strong>Children's Privacy</strong>
-          <br/><br/>
-          The App is not intended for children under the age of 13. We do not knowingly collect personal information from children under 13. If you are a parent or guardian and believe that your child has provided personal information, please contact us, and we will promptly delete such information from our records.
-          <br/><br/>
-          <strong>Changes to This Privacy Policy</strong>
-          <br/><br/>
-          We reserve the right to update or modify this Privacy Policy at any time. We will notify you of any changes by posting the updated Privacy Policy within the App. Your continued use of the App after any modifications indicate your acceptance of the updated Privacy Policy.
-          <br/><br/>
-          <strong>Contact Us</strong>
-          <br/><br/>
-          If you have any questions, concerns, or suggestions regarding this Privacy Policy, please contact us at tastyfy.tech@gmail.com.
-        </div>
-      </div>
-
     </div>
   );
 }

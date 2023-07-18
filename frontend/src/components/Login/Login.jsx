9e75a326ac1ed5a1f690a60c9522ac3d0573f25f
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-07-18 20:33:10 +0200

    fix plot

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import VectorSmartObject1Image from 'src/assets/images/start1_Vector_Smart_Object_1.png';
 import Vector1Image from 'src/assets/images/start1_Vector_1.png';

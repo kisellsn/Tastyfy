3ef24eebf0501727816f69608ec04b64c0731d21
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-07-28 22:19:33 +0200

    lie

--- frontend/src/components/Callback.jsx
+++ frontend/src/components/Callback.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import VectorSmartObject1Image from 'src/assets/images/start1_Vector_Smart_Object_1.png';
+import Vector1Image from 'src/assets/images/start1_Vector_1.png';
+import './Login/Login.scss'
 
 
 const Callback = () => {
@@ -22,7 +25,33 @@ const Callback = () => {
       }, []);
 
     return (
-        <></>
+        <div id='main'>
+        <img
+          id='VectorSmartObject1'
+          src={VectorSmartObject1Image}
+          loading="eager"
+          alt={'Vector Smart Object 1'}
+        />
+        <div className='container'>
+  
+          <div id='Tastyfy-title'>{`Tastyfy`}</div>
+  
+          <div className='login-body'>
+            <div id='Tastyfy-info'>
+              <span id='Tastyfy-info-title'>{`Tastyfy`}</span>
+              <span id='Tastyfy-info-info'>{` is a music preference analyzer that helps users learn more about their taste and discover new music that matches their personal preferences.`}</span>
+            </div>
+  
+            <div id='login-button'>
+              <div id='button-click'>
+                <div id='LogIn'>{`Log In`}</div>
+                <img id='SpotifyImg' src={Vector1Image} loading="lazy" alt={'Vector'} />
+              </div>
+            </div>
+  
+          </div>
+        </div>
+      </div>
     )
 };
 

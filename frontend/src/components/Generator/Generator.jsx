e5c13dbaeb65868ceb5e9ca6bfad77919a434e74
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-07-29 23:37:57 +0200

    playlists

--- frontend/src/components/Generator/Generator.jsx
+++ frontend/src/components/Generator/Generator.jsx
@@ -5,13 +5,17 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../Header/Header';
 import { clearLocalStorage, getUserFromStorage, storeUser } from 'src/util/local';
 import { usePlaylistContext } from 'src/context/playlistContext';
+import GeneratedTrack from './GeneratedTrack';
+import { addImage, createPlaylist, generateTracks } from 'src/util/functions';
 
 
 function Generator(props) {
     const [userInfo, setUserInfo] = useState('');
     const navigate = useNavigate();
     const startRef = useRef(Date.now());
-    const {playlist} = usePlaylistContext();
+    const {playlist, removeItem} = usePlaylistContext();
+
+
     useEffect(() => {
         const checkAuthorization = () => {
           const user = getUserFromStorage();
@@ -23,14 +27,11 @@ function Generator(props) {
             setUserInfo(user);
           }
         };
-
         const checkPlaylist = () => {
             if(!playlist || playlist.length !== 5)navigate('/playlists');
         }
-    
         checkAuthorization();
         checkPlaylist(); 
-        
       }, [navigate, playlist]);
 
     let url = ''
@@ -40,7 +41,50 @@ function Generator(props) {
       const url = userInfo.external_urls.spotify;
       window.location.href = url;
     };
+    const [flag, setFlag] = useState(0);
+    const [tracks, setTracks] = useState([]);
 
+    useEffect(() => {
+        const fetchTracks = async() =>{
+            try {
+                setFlag(1);
+                const arrayOfObjects = playlist.map((item) => item.song);
+                const options = await generateTracks(arrayOfObjects);
+                setTracks(options);
+                setFlag(0);
+            } catch (error) {
+                // Handle any errors that might occur during the Promise resolution
+                console.error("Error fetching playlist songs:", error);
+            }
+        }
+        fetchTracks();
+      }, [playlist]);
+
+
+
+    const [inputName, setInputName] = useState('');
+    const [inputDescription, setInputDescription] = useState('');
+    const [inputFile, setInputFile] = useState('');
+
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        setInputFile(file);
+      };
+    const handleBack = () =>{
+        navigate('/playlists')
+    }
+    const handleCreate = async() => {
+        try {
+            const id = await createPlaylist(inputName, inputDescription, userInfo.id, tracks);
+            // if(inputFile)addImage(URL.createObjectURL(inputFile), id)
+            navigate('/playlists')
+
+
+        } catch (error) {
+            // Handle any errors that might occur during the Promise resolution
+            console.error("Error fetching playlist songs:", error);
+        }
+    }
 
     return (
         <div id='generator' className={props.className}>
@@ -49,6 +93,87 @@ function Generator(props) {
             <div id='circle2'></div>
             <div id='circle3'></div>
             <div className='body'>
+                <div className='generatorContainer'>
+                    <div className='playlistData'>
+                        <label className='data_img'>
+                            {inputFile ? (
+                                <img src={URL.createObjectURL(inputFile)} alt="Uploaded" />
+                            ) : (
+                                <svg width="45%" height="45%" viewBox="0 0 96 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clipPath="url(#clip0_709_49)">
+                                    <path d="M14.229 6.70787C12.798 5.97419 12 6.58425 12 8.06237V77.9374C12 79.4155 12.798 80.0229 14.229 79.2919L82.332 44.3329C83.766 43.5992 83.859 42.4006 82.425 41.6669L14.229 6.70787Z" stroke="#605759" strokeOpacity="0.55" strokeWidth="2" strokeMiterlimit="10"/>
+                                    <path d="M91.3848 75.481H67.3848" stroke="#605759" strokeOpacity="0.55" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round"/>
+                                    <path d="M79.3848 64.731V86.231" stroke="#605759" strokeOpacity="0.55" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round"/>
+                                    </g>
+                                    <defs>
+                                    <clipPath id="clip0_709_49">
+                                    <rect width="96" height="86" fill="white"/>
+                                    </clipPath>
+                                    </defs>
+                                </svg>
+                            )}
+                            <input
+                                type="file"
+                                id="upload-input"
+                                style={{ display: 'none' }}
+                                onChange={handleFileUpload}
+                                accept="image/*"
+                            />
+                        </label>
+                        <div className='data_name'>                        
+                            <form onSubmit={(e) => e.preventDefault()}>
+                                <input 
+                                type="text" 
+                                name="text" 
+                                autoComplete="off"
+                                value={inputName}
+                                placeholder='Name'
+                                onChange={(e) => setInputName(e.target.value)}
+                                />
+                            </form>
+                        </div>
+                        <div className='data_description'>
+                            <form onSubmit={(e) => e.preventDefault()}>
+                                    <textarea 
+                                    type="text" 
+                                    name="text" 
+                                    autoComplete="off"
+                                    value={inputDescription}
+                                    placeholder='Description'
+                                    onChange={(e) => setInputDescription(e.target.value)}
+                                    />
+                                </form>
+                        </div>
+                    </div>
+                    <div className='generatedTracks'>
+                        <div className='scrollList'>
+                            {flag === 0 ? (
+                                <>
+                                    {tracks !== '' ? (
+                                        <>
+                                        {tracks.map((song, index) => (
+                                            <GeneratedTrack key={index} song={song} removeItem={removeItem}/>
+                                        ))}
+                                        </>
+                                    ):(
+                                        <p>We don`t have you songs.</p>
+                                    )}
+                                </>
+                                ):(
+                                    <p>Loading data...</p>
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className='buttons'>
+                    <div className='buttonCreate'>
+                        <button className={playlist.length === 5 ? '' : 'unactive_button'} onClick={handleCreate}>Create</button>
+                    </div>
+                    <div className='buttonCreate'>
+                        <button className={playlist.length === 5 ? '' : 'unactive_button'} onClick={handleBack}>Back</button>
+                    </div>
+                </div>
             </div>
             
         </div>

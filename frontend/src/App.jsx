f9bf7f037e2541667aba0ff8aeefcef0fc5d0553
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-08-29 21:32:44 +0200

    remove trash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
-import questTheme from 'src/MyDesignSystemLightTheme';
 import Login from 'src/components/Login/Login';
 import Footer from "./components/Footer/footer";
 import About from './components/About/About';
@@ -17,7 +16,7 @@ function App() {
   return (
       <HelmetProvider>
         <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={questTheme}>
+          <ThemeProvider >
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/menu" element={<Menu />} />

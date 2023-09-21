85142fb25d7b16d0fdbae790b7128f185cf3babb
Sonya Kondratskaya <sonyakondratskaya@gmail.com>
2023-09-21 12:40:04 +0200

    fix

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=24)
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
+@app.errorhandler(500)
+def not_found(e):
+    return app.send_static_file('index.html')
 
 @app.route('/')
 def index():
